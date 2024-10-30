2a5f8bfd558c81128bad4f3ee5c256a6067ff435
Yiska <jaden.christie@outlook.com>
2024-10-30 15:41:02 -0300

    Implement POST /api/player to create a player

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Player from './models/player.model.js'
 
 dotenv.config();
 
@@ -8,6 +9,24 @@ const app = express();
 const PORT = process.env.PORT||5000;
 
 app.use(express.json());
+
+app.post('/api/player', async (req, res) => {
+    const player = req.body;
+
+    if (!player.name || !player.skill_level) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
+    }
+
+    const newPlayer = new Player(player);
+
+    try {
+        await newPlayer.save();
+        res.status(201).json({ success: true, data: newPlayer });
+    } catch (error) {
+        console.log(`Error in Create player: ${error.message}`);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server started at 127.0.0.1:${PORT}`);
